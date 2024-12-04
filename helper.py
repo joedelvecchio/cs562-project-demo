c00e3beb46987fc06142e52363503323151d7012
joedelvecchio <73077125+joedelvecchio@users.noreply.github.com>
2024-12-04 18:58:17 -0500

    need manual input function

--- helper.py
+++ helper.py
@@ -11,16 +11,16 @@ def create_mf_struct(file):
             line = contents[idx]
 
             if line == 'SELECT ATTRIBUTE(S):':
-                MF_Struct['select'] = contents[idx + 1].strip()
+                mf_struct['select'] = contents[idx + 1].strip()
                 idx += 1
             elif line == 'NUMBER OF GROUPING VARIABLES(n):':
-                MF_Struct['groupingVariables'] = int(contents[idx + 1].strip())
+                mf_struct['groupingVariables'] = int(contents[idx + 1].strip())
                 idx += 1
             elif line == 'GROUPING ATTRIBUTES(V):':
-                MF_Struct['groupingAttributes'] = contents[idx + 1].strip()
+                mf_struct['groupingAttributes'] = contents[idx + 1].strip()
                 idx += 1
             elif line == 'F-VECT([F]):':
-                MF_Struct['listOfAggregateFuncs'] = [agg.strip().lower() for agg in contents[idx + 1].split(',')]
+                mf_struct['listOfAggregateFuncs'] = [agg.strip().lower() for agg in contents[idx + 1].split(',')]
                 idx += 1
             elif line == 'SELECT CONDITION-VECT([C]):':
                 conditions = []
@@ -28,10 +28,10 @@ def create_mf_struct(file):
                 while idx < len(contents) and contents[idx] != 'HAVING CLAUSE (G):':
                     conditions.append(contents[idx].strip())
                     idx += 1
-                MF_Struct['selectConditionVector'] = conditions
+                mf_struct['selectConditionVector'] = conditions
                 idx -= 1  # Adjust index since HAVING CLAUSE will also be processed
             elif line == 'HAVING CLAUSE (G):':
-                MF_Struct['havingClause'] = contents[idx + 1].strip()
+                mf_struct['havingClause'] = contents[idx + 1].strip()
                 idx += 1
 
             idx += 1
