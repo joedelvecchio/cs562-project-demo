134e3ad2bf7dc12d820e96166e3d98d7528bad41
joedelvecchio <73077125+joedelvecchio@users.noreply.github.com>
2024-12-10 17:44:27 -0500

    framework for steps 1 and 2

--- helper.py
+++ helper.py
@@ -1,47 +1,44 @@
+import psycopg2
+
 #reading from file
 def read_file(file):
-    mf_struct = {}
     try:
         # Read file contents
         with open(file_path, 'r') as f:
             contents = f.read().splitlines()
 
         # Parse the contents
+        select = ""
+        grouping_variables = ""
+        grouping_attributes = ""
+        list_of_aggregates = ""
+        select_condition = ""
+        having_clause = ""
         idx = 0
         while idx < len(contents):
             line = contents[idx]
-
             if line == 'SELECT ATTRIBUTE(S):':
-                mf_struct['select'] = contents[idx + 1].strip()
-                idx += 1
+                select = lines[x+1].replace(" ", "") 
+                x += 2
             elif line == 'NUMBER OF GROUPING VARIABLES(n):':
-                mf_struct['groupingVariables'] = int(contents[idx + 1].strip())
-                idx += 1
+                grouping_variables = lines[x+1].replace(" ", "")
+                x += 2
             elif line == 'GROUPING ATTRIBUTES(V):':
-                mf_struct['groupingAttributes'] = contents[idx + 1].strip()
-                idx += 1
+                grouping_attributes = lines[x+1].replace(" ", "")
+                x += 2
             elif line == 'F-VECT([F]):':
-                mf_struct['listOfAggregateFuncs'] = [agg.strip().lower() for agg in contents[idx + 1].split(',')]
-                idx += 1
+                list_of_aggregates = lines[x+1].replace(" ", "")
+                x += 2
             elif line == 'SELECT CONDITION-VECT([C]):':
-                conditions = []
-                idx += 1
-                while idx < len(contents) and contents[idx] != 'HAVING CLAUSE (G):':
-                    conditions.append(contents[idx].strip())
-                    idx += 1
-                mf_struct['selectConditionVector'] = conditions
-                idx -= 1  # Adjust index since HAVING CLAUSE will also be processed
+                select_condition = lines[x+1].replace(" ", "")
+                x += 2
             elif line == 'HAVING CLAUSE (G):':
-                mf_struct['havingClause'] = contents[idx + 1].strip()
-                idx += 1
+                having_clause = lines[x+1]
+                x += 2
+    except:
+        print("Error processing file")
+        read_file(file)
 
-            idx += 1
-
-    except (IOError, ValueError, IndexError) as e:
-        print(f"Error processing file: {e}")
-        return None
-
-    return mf_struct
         
 #manual input from user
 def user_input():
@@ -53,13 +50,13 @@ def user_input():
     having_clause = input("Enter the HAVING clause: ")
 
 #if user wants to read from file or provide input
-def get_phi_args():
+def get_phi_args(file):
     print("Select one of the options when prompted: ")
     print("Enter '1' to read from a file")
     print("Enter '2' to input manually")
     choice = input("Enter your choice: ")
     if choice == "1":
-        read_file()
+        read_file(file)
         return
     elif choice == "2":
         user_input()
@@ -70,8 +67,21 @@ def get_phi_args():
 
 #connect to database 
 def db_connect():
+        db = psycopg2.connect(
+        host = '',
+        dbname = '',
+        user = '',
+        password = '',
+        port = 
+    )
+    cursor = db.cursor()
+    return cursor, db
 
 #initialize the database
-def db_init():
-
+def db_init(db):
+    init = open("load_sales_10000_table (NEW).sql", "r")
+    for line in init:
+        db.execute(line)
+    init.close()
+    
 #generate code for H table 
